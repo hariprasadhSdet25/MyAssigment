7134f74bcaeb7c4e0d3af2d707678b978248e239
hariprasadhSdet25 <hariprasadh86@gmail.com>
2026-03-06 12:08:52 +0530

    Add files via upload
    Assignments for review

---
+++ Week_1/Num_Types.js
@@ -0,0 +1,13 @@
+
+let num= 0
+
+if(num<0){
+    console.log("Given num is Negative value " +num);
+}
+else if(num>0)
+{console.log("Given num is Positive value "+num);
+}
+    
+else
+{console.log("Given number value is Zero "+num);
+}
---
+++ Week_1/Primitive_DataTypes.js
@@ -0,0 +1,28 @@
+// Primitive Data types
+
+let custName = "Hariprasadh";
+let accNumber = 685954125789563254n;
+let custId = 85030203
+let accholder = true
+let active
+let intRate = 8.5
+var houseLoan = null
+
+console.log('Account Holder name is  ' + custName + " ");
+console.log("Data type of this field is " + typeof (custName));
+
+console.log("Account Number is  " + accNumber);
+console.log("Data type of this field is " + typeof (accNumber));
+
+console.log("Customer ID is  " + custId);
+console.log("Data type of this field is " + typeof (custId));
+
+console.log("Data type of this active is " + typeof (active));
+
+console.log('Interest rate for HL ' + intRate);
+
+console.log("Data type of this field is " + typeof (intRate));
+
+console.log('Data type of Houseloan is  ' + houseLoan);
+
+
